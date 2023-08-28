7cf1772c05825c5ed8dffe5d46fc1a25ebecc23d
Normanjpg <normanjpg@gmail.com>
2023-08-29 00:18:18 +0700

    signup page

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,10 @@
+import SignupForm from "./SignupForm"
+
+export default function SignupPage() {
+
+    return (
+        <div className='bg-white-500 w-screen h-screen flex items-center'>
+            <SignupForm />
+        </div>
+    )
+}
